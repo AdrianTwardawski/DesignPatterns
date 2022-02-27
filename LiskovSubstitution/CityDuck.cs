29d7f0896e718781561a8ab429638b736fbd465a
adriantwardy <adriantwardawski@gmail.com>
2022-02-27 21:35:31 +0100

    Liskov Substitution

---
+++ LiskovSubstitution/CityDuck.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiskovSubstitution
+{
+    public class CityDuck : Duck
+    {
+        public override void Fly()
+        {
+            Console.WriteLine("City duck flies");
+        }
+
+        public override void Qwack()
+        {
+            Console.WriteLine("City duck Qwack");
+        }
+
+        public override void Swim()
+        {
+            Console.WriteLine("City duck Swim");
+        }
+    }
+}
---
+++ LiskovSubstitution/MountainDuck.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiskovSubstitution
+{
+    public class MountainDuck : Duck
+    {
+        public override void Fly()
+        {
+            Console.WriteLine("Mountain duck flies");
+        }
+
+        public override void Qwack()
+        {
+            Console.WriteLine("Mountain duck Qwack");
+        }
+
+        public override void Swim()
+        {
+            Console.WriteLine("Mountain duck Swim");
+        }
+    }
+}
