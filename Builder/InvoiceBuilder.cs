a68f0134413cdf0a42d6a840dbd4983de4596ddc
adriantwardy <adriantwardawski@gmail.com>
2022-02-28 16:22:16 +0100

    Builder

---
+++ Builder/InvoiceBuilder.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Builder
+{
+    public class InvoiceBuilder
+    {
+        private Invoice _invoice = new Invoice();
+
+        public Invoice Build()
+        {
+            return _invoice;
+        }
+
+        public InvoiceBuilder setVendor(string vendor)
+        {
+            _invoice.Vendor = vendor;
+            return this;
+        }
+
+        public InvoiceBuilder setVendee(string vendee)
+        {
+            _invoice.Vendee = vendee;
+            return this;
+        }
+
+        public InvoiceBuilder setNote(string note)
+        {
+            _invoice.Note = note;
+            return this;
+        }
+
+        public InvoiceBuilder setLineItems(IEnumerable<string> lineItems)
+        {
+            _invoice.LineItems = lineItems;
+            return this;
+        }
+
+        public InvoiceBuilder setInvoiceNumber(string number)
+        {
+            _invoice.Number = number;
+            return this;
+        }
+
+        public InvoiceBuilder setDate(DateTime date)
+        {
+            _invoice.Date = date;
+            return this;
+        }
+    }
+}
---
+++ Builder/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Builder
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new InvoiceBuilder();
+
+            Invoice invoice = builder.setDate(new DateTime(2022, 1, 12))
+                .setInvoiceNumber("A1")
+                .setVendor("Amazon")
+                .setVendee("Vendee")
+                .setLineItems(new List<string>() { "Line item1", "Line item2" })
+                .Build();
+
+            Console.WriteLine(invoice.Vendee);
+            Console.WriteLine(invoice.Vendor);
+        }
+    }
+}
