70d762405efbdff7f4aab39e43f8224cd1433628
adriantwardy <adriantwardawski@gmail.com>
2022-02-28 11:19:54 +0100

    InterfaceSegregation

---
+++ InterfaceSegregation/Cannon.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceSegregation
+{
+    public class Cannon : IPrinter, IFaxer
+    {
+        public void Fax(string content)
+        {
+            Console.WriteLine("Cannon print fax");
+        }
+
+        public void PrintColor(string content)
+        {
+            Console.WriteLine("Cannon print color");
+        }
+
+        public void PrintGrey(string content)
+        {
+            Console.WriteLine("Cannon print grey");
+        }
+      
+    }
+}
---
+++ InterfaceSegregation/HpLaserJet.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceSegregation
+{
+    public class HpLaserJet : IPrinter, IScanner, IFaxer
+    {
+        public void Fax(string content)
+        {
+            Console.WriteLine("Hp laser jet print fax");
+        }
+
+        public void PrintColor(string content)
+        {
+            Console.WriteLine("Hp laser jet print color");
+        }
+
+        public void PrintGrey(string content)
+        {
+            Console.WriteLine("Hp laser jet print grey");
+        }
+
+        public void Scan(string content)
+        {
+            Console.WriteLine("Hp laser jet print scan");
+        }
+    }
+}
