d30cb730da3a2e00938a1b3b8cff22a2c8bc7660
adriantwardy <adriantwardawski@gmail.com>
2022-03-01 12:28:39 +0100

    Prototype (klonowanie płytkie)

---
+++ Prototype/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle c1 = new Circle()
+            {
+                Radius = 10,
+                X = 1,
+                Y = 2,
+                Border = new Border()
+                {
+                    Color = "Blue",
+                    Size = "Big"
+                }
+            };
+
+            Circle c2 = (Circle)c1.Clone();
+
+            bool referenceEquals = ReferenceEquals(c1, c2);
+            Console.WriteLine($"ReferenceEquals: {referenceEquals}");
+
+            bool borderReferenceEquals = ReferenceEquals(c1.Border, c2.Border);
+            Console.WriteLine($"borderReferenceEquals: {borderReferenceEquals}");
+
+            Console.WriteLine();
+
+        }
+    }
+}
---
+++ Prototype/Rectangle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    public class Rectangle : Shape
+    {
+        public override Shape Clone()
+        {
+            return (Rectangle)this.MemberwiseClone();
+        }
+
+        public override void Render()
+        {
+            Console.WriteLine("Render rectangle");
+        }
+    }
+}
