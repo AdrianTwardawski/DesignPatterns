833a30d51a35895b5265227b39a57add64a26526
adriantwardy <adriantwardawski@gmail.com>
2022-03-04 11:02:44 +0100

    Mediator

---
+++ Mediator/Component.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mediator
+{
+    public abstract class Component
+    {
+        protected IMediator _mediator; //ustawienie referencji do mediatora
+
+        public void SetMediator(IMediator mediator) 
+        {
+            _mediator = mediator;
+        }
+    }
+}
