4a469a5cc84cb452ba35e159867215a3f00dfcc0
adriantwardy <adriantwardawski@gmail.com>
2022-03-05 19:32:02 +0100

    Structural Design Pattern - Adapter

---
+++ Adapter/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //INotificationSender notificationSender = new EmailSender();
+            INotificationSender notificationSender = new SmsSenderAdapter();
+            notificationSender.SendNotification(1, new Notification() { Title = "Test", Body = "Body" });
+        }
+    }
+
+}
---
+++ Adapter/SmsSender.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    public class SmsSender //zakładamy, że ta klasa jest klasą zewnętrzną i to dla niej przygotowujemy adapter
+    {
+        public void SendSms(string to, string text) //metoda służąca do wysyłania sms-ów
+        {
+            Console.WriteLine($"Sending SMS to : {to}");
+        }
+    }
+}
