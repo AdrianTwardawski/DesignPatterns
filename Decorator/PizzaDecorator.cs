4b487cee6d136b21e40c07fcafb34dd7bcaceada
adriantwardy <adriantwardawski@gmail.com>
2022-03-05 22:04:52 +0100

    Wzorce projektowe strukturalne - Dekorator

---
+++ Decorator/PizzaDecorator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    public abstract class PizzaDecorator : IPizza
+    {
+        private IPizza _pizza; //prywatne pole pod, którym przechowywana jest pizza, którą chcemy udekorować
+
+        protected PizzaDecorator(IPizza pizza)
+        {
+            _pizza = pizza;
+        }
+        public virtual double CalculatePrice() //metoda wirtualna, po to aby każda implementacja dekoratora była w stanie wprowadzić
+                                               //własną implementację tej metody
+        {
+            return _pizza.CalculatePrice();
+        }
+    }
+}
